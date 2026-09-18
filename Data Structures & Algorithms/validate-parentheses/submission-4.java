a4b189a9cbd3f44a3a4f3b0e88b2e6fde5f000c8
Prisha67 <prishaguru67@gmail.com>
2026-09-19 04:38:56 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> sc = new Stack<>();
+        int num = s.length();
+        if (num%2!= 0){return false;}
+        for (int i =0 ; i < num ; i++){
+            char cur = s.charAt(i);
+            if (cur=='('||cur=='['||cur=='{'){
+                sc.push(cur);
+            }else{
+                if (sc.isEmpty()){return false;}
+                if (cur ==')' && sc.peek()=='(') {sc.pop();continue;}
+                else if (cur =='}' && sc.peek()=='{') {sc.pop();continue;}
+                else if (cur ==']' && sc.peek()=='[') {sc.pop();continue;}
+                else{return false;}
+            }
+        }
+        return sc.isEmpty();
+    }
+}
